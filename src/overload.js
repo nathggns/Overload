7e24c4632ded96266b2b4cd915f466e924c6d039
Nathaniel Higgins <nat@nath.is>
2013-07-22 18:04:06 +0100

    Initial commit

---
+++ src/overload.js
@@ -0,0 +1,26 @@
+(function (root, name, dependencies, factory) {
+
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(name, ['exports'].concat(dependencies), factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        factory.apply(root, [exports].concat(dependencies.map(function(dependency) {
+            return require(dependency);
+        })));
+    } else {
+        // Browser globals
+        factory.apply(root, [root[name] = {}].concat(dependencies.map(function(dependency) {
+            return root[dependency];
+        })));
+    }
+}(this, 'extend', ['heir'], function (exports, heir) {
+
+    'use strict';
+
+    // For tests
+    exports.heir = heir;
+
+}));
